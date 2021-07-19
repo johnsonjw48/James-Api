f204ab82e4a0a1ec5de98d46078ac511df10e5bb
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2021-07-19 02:59:45 +0200

    second

--- Controller/postsController.js
+++ Controller/postsController.js
@@ -3,3 +3,14 @@ const express = require ('express');
 const router= express.Router();
 
 const {PostsModele} = require('../Modèle/postsModèle.js');
+
+router.get('/', (req, res) => {
+  PostsModele.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data : " + err);
+  })
+});
+
+
+module.exports = router;
+
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const app = express();
 require('./Modèle/dbConfig.js');
+const postsController = require('./Controller/postsController');
+
 
 app.listen(5500, ()=> console.log('Server Started:5500'));
 
+app.use('/',  postsController);
